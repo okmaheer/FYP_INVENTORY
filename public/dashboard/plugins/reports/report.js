a562c0c1ccff26a711405fc82aeaff568a9b31b9
okmaheer <86462580+okmaheer@users.noreply.github.com>
2022-07-04 16:33:59 +0500

    CHNAGES

--- public/dashboard/plugins/reports/report.js
+++
@@ -1,8 +0,0 @@
-function printDiv(divName) {
-    var printContents = document.getElementById(divName).innerHTML;
-    var originalContents = document.body.innerHTML;
-    document.body.innerHTML = printContents;
-    document.body.style.marginTop = "0px";
-    window.print();
-    document.body.innerHTML = originalContents;
-}
--- resources/views/includes/datatable-css.blade.php
+++
@@ -1,3 +0,0 @@
-<link href="{{ asset('dashboard/plugins/datatables/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css"/>
-<link href="{{ asset('dashboard/plugins/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css"/>
-<link href="{{ asset('dashboard/plugins/datatables/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css"/>
